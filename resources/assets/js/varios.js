597c7b2e69552b641e13fdddab18707d5a2ae9e2
Andres Felipe Zapata Mesa <andres.mesa@quipux.com>
2019-10-19 01:19:46 -0500

    crea dashboard y dasboard-historial

---
+++ resources/assets/js/varios.js
@@ -0,0 +1,16 @@
+/* ----toggle------ */
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+  });
+
+/*   $(document).ready(function() {
+    $('#example').DataTable( {
+        "paging":   false,
+        "ordering": false,
+        "info":     false
+    } );
+} ); */
+
+
+    
